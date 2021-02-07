70cc3c4cd1f35fe8f9f031df2b3e7bc509a0f567
Carlos Lavieri <carlos_lavieri@pm.me>
2021-02-07 19:26:57 -0400

    Version 0.0.1

---
+++ apps/app.js
@@ -0,0 +1,8 @@
+console.log(window.location.pathname);
+
+window.onload = function(){
+    var contenedor = document.getElementById('load');
+
+    contenedor.style.visibility = "hidden";
+    contenedor.style.opacity = '0';
+}
